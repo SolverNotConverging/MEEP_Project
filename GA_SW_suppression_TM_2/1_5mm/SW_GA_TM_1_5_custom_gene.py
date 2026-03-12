5e4dcec358f0c6f60945a29fe43a136477904139
Life is Hard <mostima0@outlook.com>
2026-03-12 11:30:25 +0000

    Data update

--- GA_SW_suppression_TM_2/1_5mm/SW_GA_TM_1_5_custom_gene.py
+++ GA_SW_suppression_TM_2/1_5mm/SW_GA_TM_1_5_custom_gene.py
@@ -36,7 +36,13 @@ os.makedirs(RESULTS_DIR, exist_ok=True)
 # USER_GENOMES = [[1, 3, 3]]
 # USER_GENOMES = [[1, 3, 3], [3, 1, 3]]
 
-USER_GENOMES = [[2, 2, 2, 2, 2],
+USER_GENOMES = [[1, 1, 1, 1, 3],
+                [1, 1, 1, 3, 3],
+                [1, 1, 1, 2, 2],
+                [1, 1, 1, 2, 2],
+                [2, 2, 2, 2, 1],
+                [2, 2, 1, 1, 1],
+                [2, 2, 0, 1, 1],
                 ]
 
 
